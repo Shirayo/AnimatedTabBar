be4c657757565862fd57d1d55e67527a7056cb84
Shirayo <iphoje@gmail.com>
2022-04-25 11:00:07 +0300

    Initial Commit

---
+++ AnimatedTabBar/AnimatedTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedTabBarApp.swift
+//  AnimatedTabBar
+//
+//  Created by Vasili on 25.04.22.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
